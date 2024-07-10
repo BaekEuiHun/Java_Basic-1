baa215cb35bd6e32d3ff553bb5915a575afbe8fb
BaekEuiHun <euihunbaek@gmail.com>
2024-07-10 20:51:28 +0900

    init

--- src/variable/Var2.java
+++ src/variable/Var2.java
@@ -3,12 +3,13 @@ package variable;
 public class Var2 {
     public static void main(String[] args) {
 
-        int a = 100; //정수
-        double b = 10.5; //실수
-        boolean c = true; //불리언 / 참, 거짓
-        char d = 'A'; //문자 하나
-        String e = "Hello Java"; //문자열을 다루기 위한 특별한 타입
+        int a = 100; //정수 리터럴
+        double b = 10.5; //실수 리터럴
+        boolean c = true; //불리언 리터럴 / 참, 거짓
+        char d = 'A'; //문자 하나 리터럴
+        String e = "Hello Java"; //문자열을 다루기 위한 특별한 타입, 문자열 리터럴
 
+        //리터럴 : 고정된 값
 
         System.out.println("a = " + a);
         System.out.println("b = " + b);
