00942a2d5d4d0f238709289885426456b5d60991
BaekEuiHun <euihunbaek@gmail.com>
2024-07-10 19:29:59 +0900

    init

---
+++ src/variable/Var1.java
@@ -0,0 +1,17 @@
+package variable;
+
+public class Var1 {
+    public static void main(String[] args) {
+
+        int a; //변수 선언
+        a = 1; //변수 초기화
+        System.out.println("a = " + a);
+
+        int b = 2;
+        System.out.println("b = " + b);
+
+        int c = 3, d = 4;
+        System.out.println("c = " + c);
+        System.out.println("c = " + d);
+    }
+}
