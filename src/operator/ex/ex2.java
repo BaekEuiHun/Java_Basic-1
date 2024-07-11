d2486d23a88a31ae31a3d9fb99022c1e875ac1cd
BaekEuiHun <euihunbaek@gmail.com>
2024-07-11 09:12:46 +0900

    init

---
+++ src/operator/ex/ex2.java
@@ -0,0 +1,16 @@
+package operator.ex;
+
+public class ex2 {
+    public static void main(String[] args) {
+
+        double va1 = 1.5;
+        double va2 = 2.5;
+        double va3 = 3.5;
+
+        double sum = va1 + va2 + va3;
+        double avg = sum / 3;
+
+        System.out.println("sum = " + sum);
+        System.out.println("avg = " + avg);
+    }
+}
