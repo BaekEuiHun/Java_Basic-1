255dc1e50ed050c9e35887eaca174f4abcbcdc73
BaekEuiHun <euihunbaek@gmail.com>
2024-07-11 13:29:13 +0900

    init

---
+++ src/cond/ex7.java
@@ -0,0 +1,13 @@
+package cond;
+
+public class ex7 {
+    public static void main(String[] args) {
+
+        int x = 3;
+
+        String result = (x % 2 == 0) ? "짝수" : "홀수";
+
+        System.out.println("x = " + x + " , " +  result + "입니다.");
+
+    }
+}
