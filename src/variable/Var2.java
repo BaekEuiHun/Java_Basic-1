55c97dc24ae5404a7427eee921b7d8dcae99a237
BaekEuiHun <euihunbaek@gmail.com>
2024-07-20 18:42:23 +0900

    Update Var2.java

--- src/variable/Var2.java
+++ src/variable/Var2.java
@@ -5,7 +5,7 @@ public class Var2 {
 
         int a = 100; //정수 리터럴
         double b = 10.5; //실수 리터럴
-        boolean c = true; //불리언 리터럴 / 참, 거 짓
+        boolean c = true; //불리언 리터럴 / 참, 거짓
         char d = 'A'; //문자 하나 리터럴
         String e = "Hello Java"; //문자열을 다루기 위한 특별한 타입, 문자열 리터럴
 
