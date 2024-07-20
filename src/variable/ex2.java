d4e8e1d5c3e1e063cf1dd70aa2b67c14f7f1af6f
BaekEuiHun <euihunbaek@gmail.com>
2024-07-20 18:42:04 +0900

    Update ex2.java

--- src/variable/ex2.java
+++ src/variable/ex2.java
@@ -1,7 +1,7 @@
 package variable;
 
 public class ex2 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
         int num1 = 10;
         int num2 = 20;
